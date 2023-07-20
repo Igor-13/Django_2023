d38ebfbf57c00ca0360245db54acc00c2a5c2aa3
Igor-13 <96l.i.s69@mail.ru>
2023-07-21 01:19:43 +1000

    lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,22 @@ from django.views.generic import TemplateView
 
 
 class MainPageView(TemplateView):
-    template_name = "mainapp/index.html"
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context["news_title"] = "Громкий новостной заголовок"
+        
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        
+        context["range"] = range(5)
+        
+        return context
+
 
 
 class NewsPageView(TemplateView):
@@ -23,3 +38,13 @@ class DocSitePageView(TemplateView):
 
 class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
+
+
+
+class NewsWithPaginatorView(NewsPageView):
+    
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+
+        return context
